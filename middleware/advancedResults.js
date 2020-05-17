4bb94129f46b8abb42a54af272f371e2afcbb79a
Dprash <prashantdixit600@gmail.com>
2020-05-18 04:08:02 +0530

    Deploy to Heroku

--- middleware/advancedResults.js
+++ middleware/advancedResults.js
@@ -38,7 +38,7 @@ const advancedResults = (model, populate) => async (req, res, next) => {
 
   // Pagination
   const page = parseInt(req.query.page, 10) || 1;
-  const limit = parseInt(req.query.limit, 10) || 500;
+  const limit = parseInt(req.query.limit, 10) || 15000;
   const startIndex = (page - 1) * limit;
   const endIndex = page * limit;
   const total = await model.countDocuments();
